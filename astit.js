8c06c10c5e30245f07bd91f58f1f06c09de33e55
AstitvaTomar14 <tomarastitva18@gmail.com>
2024-10-17 14:09:48 +0530

    js

---
+++ astit.js
@@ -0,0 +1,11 @@
+import { add, substract, PI}from "./mathUtils.js";
+
+import * as jadoo from "./mathUtis.js";
+
+import multiply from "./mathUtis.js";
+
+export {add,substract,PI,multiply};
+console.log("add:",jadoo.add(5,3));
+console.log("substract:",jadoo.substract(5,3));
+console.log("PI:",jadoo.PI(5,3));
+console.log("multiply:",jadoo.multiply(5,3));
---
+++ astitvaa.js
@@ -0,0 +1,5 @@
+const check = require("./astitva.js");
+console.log("Add:",check.add(5,3));
+console.log("Substract:",check.substract(5,3));
+console.log("PI:",check.PI(5,3));
+console.log("Multiply:",check.multiply(5,3));
